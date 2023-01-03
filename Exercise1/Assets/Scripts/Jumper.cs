e7f0e7a714ae09bf35a286b7c84bdecf4f18eeab
Lyndon Panton <LyndonPanton@gmail.com>
2023-01-03 22:09:07 +0000

    Completed problem 2
    Added code to enable game object to "jump" between random positions within a given range every second

---
+++ Exercise1/Assets/Scripts/Jumper.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    // jump location support
+    const float MinX = -8;
+    const float MaxX = 8;
+    const float MinY = -4;
+    const float MaxY = 4;
+
+    // timer support
+    const float totalJumpDelaySeconds = 1;
+    float elapsedJumpDelaySeconds = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // update timer and check if it's done
+        elapsedJumpDelaySeconds += Time.deltaTime;
+
+        if (elapsedJumpDelaySeconds >= totalJumpDelaySeconds)
+        {
+            elapsedJumpDelaySeconds = 0;
+
+            Vector2 position = transform.position;
+
+            position.x = Random.Range(MinX, MaxX);
+            position.y = Random.Range(MinY, MaxY);
+
+            transform.position = position;
+        }
+    }
+}
