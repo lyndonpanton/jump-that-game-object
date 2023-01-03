afdf31a8f0dd7e457ccf1bf3461886c348d409a2
Lyndon Panton <LyndonPanton@gmail.com>
2023-01-03 22:16:25 +0000

    Made the game object larger

--- Exercise1/Assets/Scripts/Jumper.cs
+++ Exercise1/Assets/Scripts/Jumper.cs
@@ -14,6 +14,17 @@ public class Jumper : MonoBehaviour
     const float totalJumpDelaySeconds = 0.1f;
     float elapsedJumpDelaySeconds = 0;
 
+    // Start is called when the object is created
+    void Start()
+    {
+        Vector2 scale = transform.localScale;
+
+        scale.x *= 3;
+        scale.y *= 3;
+
+        transform.localScale = scale;
+    }
+
     // Update is called once per frame
     void Update()
     {
